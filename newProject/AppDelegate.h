a769982cc01a400482676c9509f103f79a79b290
ZSMJ1991 <1175569871@qq.com>
2022-01-11 10:08:40 +0800

    初始化

---
+++ newProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newProject
+//
+//  Created by Mac on 2022/1/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+
+
+
+@end
+
